4e0abfdc4da9116d3a4ab34b8a5faa3098649ce1
FrancineBlanc <francine.blanc@gmail.com>
2021-09-14 11:42:09 +0100

    adds functionality for taking command line args, tidies up formatting

--- directory.rb
+++ directory.rb
@@ -1,15 +1,59 @@
 @students = []
 
+def print_menu
+  puts "1. Input the students."
+  puts "2. Show the students."
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "5. Display students beginning with a particular letter"
+  puts "6. Display students with short names"
+  puts "9. Exit"
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "5"
+    begins_with
+  when "6"
+    shorter_names
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again."
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   until name.empty? do
     @students << {name: name, cohort: :november}
     string = "Now we have #{@students.count}"
     puts @students.count == 1 ? "#{string} student" : "#{string} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def print_header
@@ -32,9 +76,41 @@ def print_footer
   puts @students.count == 1? "#{string} student" : "#{string} students"
 end
 
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exist?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist"
+    exit
+  end
+end
+
 def begins_with
   puts "Please enter the letter that the student name begins with"
-  char = gets.downcase.chomp
+  char = STDIN.gets.downcase.chomp
   matched_names = @students.select { |student| student[:name].downcase.start_with?(char) }
   unless matched_names.empty?
     puts "The following students start with the letter \"#{char.upcase}\""
@@ -54,62 +130,5 @@ def shorter_names
   end
 end
 
-def print_menu
-  puts "1. Input the students."
-  puts "2. Show the students."
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def process(selection)
-  case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9"
-    exit
-  else
-    puts "I don't know what you meant, try again."
-  end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
-def save_students
-  #open the file for writing
-  file = File.open("students.csv", "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
+try_load_students
 interactive_menu
